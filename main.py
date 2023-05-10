3e4d61ea8d748e9b8a51b474c027a5646c9ea2ea
santaspan <staplachan@gmail.com>
2023-05-10 22:22:45 +0100

    parsed file

--- main.py
+++ main.py
@@ -127,8 +127,8 @@ def translate_file(input_file, output_file, langs=["ja", "en"]):
 
 def gradio_translate(target_file, from_language, to_language):
     deepl = DeepLCLI(from_language, to_language, timeout=150000, use_dom_submit=True)
-    file_data = target_file.read()
-    file_data = file_data.decode("utf-8")
+    with open(target_file.name, "r", encoding="utf-8") as file:
+        file_data = file.read()
     print(file_data)
     subs = list(srt.parse(file_data))
     segments = [x.content for x in subs]
