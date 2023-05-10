9639c1b69c1640f5f571b7a827e109ededd85e8e
santaspan <staplachan@gmail.com>
2023-05-10 22:56:06 +0100

    split to subprocess

---
+++ deepl_tl.py
@@ -0,0 +1,84 @@
+import argparse
+import sys
+
+import srt
+from deepl import DeepLCLI
+
+delimiter = "\n---\n"
+
+def batch_deepl_text(segments: list[str], max_len=3000):
+
+    delimiter_len = len(delimiter)
+    bulk_segments = [""]
+    for i in segments:
+        new_seg = False
+        current_batch = bulk_segments[-1]
+        
+        if len(current_batch) + len(i) + delimiter_len >= max_len:
+            new_seg = True
+            current_batch = ""
+            
+        if not new_seg:
+            current_batch += delimiter
+            current_batch += i
+            bulk_segments[-1] = current_batch
+        else:
+            bulk_segments.append(i)
+    
+    return bulk_segments
+
+def batch_get_translations(deepl, segment_texts):
+    translated_segments = []
+    batched_segments = batch_deepl_text(segment_texts)
+    
+    print(len(batched_segments))
+    for count, batch in enumerate(batched_segments):
+        print(count)
+        translated = deepl.translate(batch)
+        batch_segments = translated.split(delimiter)
+        batch_segments = [x for x in batch_segments if x.strip()]
+        translated_segments.extend(batch_segments)
+    
+    return translated_segments
+def high_level():
+    srt_data = ()
+    split_translations = ()
+    
+    for i, translation_text in enumerate(split_translations):
+        srt_data[i].content = translation_text
+        
+    
+    new_srt = srt.compose(srt_data)
+    pass
+
+def translate_file(input_file, output_file, langs=["ja", "en"]):
+    deepl = DeepLCLI(langs[0], langs[1], timeout=150000, use_dom_submit=True)
+    
+    
+    with open(input_file, "r", encoding="utf-8") as file:
+        file_data = file.read()
+        
+    subs = list(srt.parse(file_data))
+    segments = [x.content for x in subs]
+    translated = batch_get_translations(deepl,segments)
+    for count, i in enumerate(translated):
+        subs[count].content = i
+        
+    new_subs = srt.compose(subs)
+    with open(output_file, "w", encoding="utf-8") as file:
+        file.write(new_subs)
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description="Translate a file from one language to another")
+
+    parser.add_argument("--filename", dest="filename", required=True, help="The path to the input file")
+    parser.add_argument("--out-filename", dest="out_filename", required=True, help="The path to the output file")
+    parser.add_argument("--from_lang", dest="from_lang", required=True, help="The language to translate from")
+    parser.add_argument("--to_lang", dest="to_lang", required=True, help="The language to translate to")
+
+    args = parser.parse_args(sys.argv[1:])
+    
+    translate_file(args.filename, args.out_filename, langs=[args.from_lang, args.to_lang])
+    
+    
--- main.py
+++ main.py
@@ -1,152 +1,30 @@
 import asyncio
+import subprocess as sp
 import time
 from dataclasses import replace
+from distutils.spawn import spawn
 from fnmatch import translate
 
 import gradio as gr
 import srt
 from deepl import DeepLCLI
 
-from_lang = "en"
-to_lang = "ja"
-
-test_file_path = "./sawagi.srt"
-
-output_path = "/content/test.srt"
-    
-    
-delimiter = "\n---\n"
-async def replace_sub(subtitle_chunk):
-    orig_lang = subtitle_chunk.content
-    translated = await deepl.translate_async(orig_lang)
-    subtitle_chunk.content = translated
-    return
-
-
-
-
-
-async def main():
-    with open(test_file_path, "r", encoding="utf-8") as file:
-        file_data = file.read()
-    
-    subs = srt.parse(file_data)
-    tasks = []
-    
-    for sub in subs:
-        tasks.append(asyncio.create_task(replace_sub(sub)))
-        await asyncio.sleep(1)
-        
-
-    # run event loop
-    asyncio.gather(*tasks)
-    
-    for sub in subs:
-        print(sub.content)
-
-def no_async_main():
-    with open(test_file_path, "r", encoding="utf-8") as file:
-        file_data = file.read()
-    
-    subs = list(srt.parse(file_data))
-    
-    for sub in subs:
-        translation = deepl.translate(sub.content)
-        sub.content = translation
-        
-    new_subs = []
-    for sub in subs:
-        new_subs.append(sub.content)
-        
-    print(new_subs)
-        
-    
-        
-        
-def batch_deepl_text(segments: list[str], max_len=3000):
-
-    delimiter_len = len(delimiter)
-    bulk_segments = [""]
-    for i in segments:
-        new_seg = False
-        current_batch = bulk_segments[-1]
-        
-        if len(current_batch) + len(i) + delimiter_len >= max_len:
-            new_seg = True
-            current_batch = ""
-            
-        if not new_seg:
-            current_batch += delimiter
-            current_batch += i
-            bulk_segments[-1] = current_batch
-        else:
-            bulk_segments.append(i)
-    
-    return bulk_segments
-
-def batch_get_translations(deepl, segment_texts):
-    translated_segments = []
-    batched_segments = batch_deepl_text(segment_texts)
-    
-    print(len(batched_segments))
-    for count, batch in enumerate(batched_segments):
-        print(count)
-        translated = deepl.translate(batch)
-        batch_segments = translated.split(delimiter)
-        batch_segments = [x for x in batch_segments if x.strip()]
-        translated_segments.extend(batch_segments)
-    
-    return translated_segments
-def high_level():
-    srt_data = ()
-    split_translations = ()
-    
-    for i, translation_text in enumerate(split_translations):
-        srt_data[i].content = translation_text
-        
-    
-    new_srt = srt.compose(srt_data)
-    pass
-
-def translate_file(input_file, output_file, langs=["ja", "en"]):
-    deepl = DeepLCLI(langs[0], langs[1], timeout=150000, use_dom_submit=True)
-    
-    
-    with open(input_file, "r", encoding="utf-8") as file:
-        file_data = file.read()
-        
-    subs = list(srt.parse(file_data))
-    segments = [x.content for x in subs]
-    translated = batch_get_translations(deepl,segments)
-    for count, i in enumerate(translated):
-        subs[count].content = i
-        
-    new_subs = srt.compose(subs)
-    with open(output_file, "w", encoding="utf-8") as file:
-        file.write(new_subs)
+default_from_lang = "en"
+default_to_lang = "ja"
+output_path = ""
 
 def gradio_translate(target_file, from_language, to_language):
-    deepl = DeepLCLI(from_language, to_language, timeout=150000, use_dom_submit=True)
-    with open(target_file.name, "r", encoding="utf-8") as file:
-        file_data = file.read()
-    print(file_data)
-    subs = list(srt.parse(file_data))
-    segments = [x.content for x in subs]
-    translated = batch_get_translations(deepl,segments)
-    for count, i in enumerate(translated):
-        subs[count].content = i
-        
-    new_subs = srt.compose(subs)
-    with open(output_path, "w", encoding="utf-8") as file:
-        file.write(new_subs)
-        
+    target_file = target_file.name
+    return spawn_new_process(target_file, from_language, to_language)
+
+def spawn_new_process(target_file, from_language, to_language):
+    cmd = ["./deepl.py", "--filename", target_file, "--out-filename", output_path, "--from_lang", from_language, "--to_lang", to_language]
+    p = sp.Popen(cmd)
+    p.wait()
     return output_path
-    
 
 def gradio():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    inputs = [gr.File(label="Upload File"), gr.Text(label="From Language"),gr.Text(label="To Language")]
+    inputs = [gr.File(label="Upload File"), gr.Text(label="From Language", value=default_from_lang),gr.Text(label="To Language", value=default_to_lang)]
     interface = gr.Interface(fn=gradio_translate,
                              inputs=inputs,
                              outputs = [
